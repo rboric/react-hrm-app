eb462236685cf1297c27e5af072419fac70ee2d3
unknown <rboric1901@gmail.com>
2023-06-18 10:09:10 +0200

    Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,33 @@
 import { Link } from "react-router-dom";
+import { Table } from "react-bootstrap";
 
 export default function Home() {
   return (
     <div className="home">
       <h1>Welcome to HRM App</h1>
       <p>Manage your human resources efficiently and effectively.</p>
-
-      <Link to="/login">Signup</Link>
+      <Table>
+        <tr>
+          <td>
+            <Link to="/login">Login</Link>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <Link to="/signup">Signup</Link>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <Link to="/signup-admin">Signup Admin</Link>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <Link to="/register-firm">Register Firm</Link>
+          </td>
+        </tr>
+      </Table>
     </div>
   );
 }
